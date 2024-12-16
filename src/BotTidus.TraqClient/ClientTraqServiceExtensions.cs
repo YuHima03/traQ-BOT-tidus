9f301a701395b473c5ae8bcf79374046a4ffea59
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:47:28 +0900

    ✨ ClientTraqServiceExtensions クラス

---
+++ src/BotTidus.TraqClient/ClientTraqServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace BotTidus.TraqClient
+{
+    public static class ClientTraqServiceExtensions
+    {
+        public static IServiceCollection AddTraqClient<TService>(this IServiceCollection collection, Action<IClientTraqBuilder> configurator)
+            where TService : class, IClientTraqService
+        {
+            ClientTraqBuilder builder = new();
+            configurator.Invoke(builder);
+            collection.TryAddSingleton<IClientTraqService>(builder.Build);
+            return collection;
+        }
+    }
+}
