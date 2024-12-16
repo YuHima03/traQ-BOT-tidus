d3ac5cc93a270d45f7e61ee914773c6d72ab0f42
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:51:03 +0900

    ✨ InteractiveBotService, IntervalBotService クラス

---
+++ src/BotTidus.BotClient/BotServices/InteractiveBotService.cs
@@ -0,0 +1,17 @@
+﻿using BotTidus.TraqBot;
+using BotTidus.TraqClient;
+using BotTidus.TraqClient.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BotTidus.BotClient.BotServices
+{
+    internal class InteractiveBotService(IServiceProvider provider) : TraqBotService(provider)
+    {
+        readonly Traq _traq = provider.GetRequiredService<IClientTraqService>().Traq;
+
+        protected override ValueTask OnMessageCreated(CancellationToken ct)
+        {
+            return base.OnMessageCreated(ct);
+        }
+    }
+}
