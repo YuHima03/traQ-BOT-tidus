8bfef4edbd306fb028f10acf9b321a434d32d764
Yusuke.T <yuhima03@gmail.com>
2024-12-16 12:46:31 +0900

    ✨ IClientTraqService インターフェースとその実装型

---
+++ src/BotTidus.TraqClient/ClientTraqService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BotTidus.TraqClient
+{
+    public class ClientTraqService(ILogger<ClientTraqService> logger) : IClientTraqService
+    {
+        readonly HttpClient _client = new();
+
+        readonly ILogger<ClientTraqService> _logger = logger;
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public Task StartAsync(CancellationToken ct)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken ct)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
