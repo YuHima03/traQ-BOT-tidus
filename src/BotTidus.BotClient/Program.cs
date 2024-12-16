ea1aa51e2878036d80a548c428dc0e69d5af9bb5
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:52:21 +0900

    ✨ 新しく入れた機能を使う

--- src/BotTidus.BotClient/Program.cs
+++ src/BotTidus.BotClient/Program.cs
@@ -1,6 +1,5 @@
-﻿using BotTidus.TraqBot;
+﻿using BotTidus.BotClient.BotServices;
 using BotTidus.TraqClient;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,6 +8,8 @@ namespace BotTidus.BotClient
 {
     internal class Program
     {
+        static readonly Uri BaseUri = new("https://q.trap.jp/");
+
         static async Task Main(string[] args)
         {
             var accessToken = Environment.GetEnvironmentVariable("BOT_ACCESS_TOKEN");
@@ -20,10 +21,14 @@ namespace BotTidus.BotClient
             var botAppBuilder = Host.CreateApplicationBuilder();
 
             botAppBuilder.Services
-                .AddHostedService<TraqBotService>()
                 .AddHttpClient()
                 .AddLogging(b => b.AddConsole())
-                .AddSingleton<IClientTraqService, ClientTraqService>();
+                .AddTraqClient<ClientTraqService>(b => b
+                    .SetBaseUri(BaseUri)
+                    .UseBearerAuthorization(accessToken)
+                )
+                .AddHostedService<InteractiveBotService>()
+                .AddHostedService<IntervalBotService>();
 
             var botApp = botAppBuilder.Build();
             await botApp.RunAsync();
