326a7b11200d1925e0d01dc7debc265b27e85bc3
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:47:03 +0900

    ✨ IClientTraqService インターフェースへの機能の追加

--- src/BotTidus.TraqClient/ClientTraqService.cs
+++ src/BotTidus.TraqClient/ClientTraqService.cs
@@ -1,27 +1,66 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using BotTidus.TraqClient.Helpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.WebSockets;
 
 namespace BotTidus.TraqClient
 {
-    public class ClientTraqService(ILogger<ClientTraqService> logger) : IClientTraqService
+    public class ClientTraqService : IClientTraqService
     {
-        readonly HttpClient _client = new();
+        readonly TraqAuthOptions _authOptions;
+        readonly HttpClient _client;
+        readonly ILogger<ClientTraqService>? _logger;
 
-        readonly ILogger<ClientTraqService> _logger = logger;
+        public Uri BaseUri { get; private init; }
+
+        public Models.Traq Traq { get; private init; }
+
+        internal ClientTraqService(IServiceProvider provider, HttpClient client, TraqAuthOptions authOptions, Uri baseUri)
+        {
+            _authOptions = authOptions;
+            _client = client;
+            _logger = provider.GetService<ILogger<ClientTraqService>>();
+
+            BaseUri = baseUri;
+            Traq = new(client) { BaseUri = baseUri };
+        }
+
+        public ClientWebSocket CreateClientWebSocket()
+        {
+            ClientWebSocket ws = new();
+
+            switch (_authOptions.AuthorizationMethod)
+            {
+                case AuthorizationMethods.Bearer:
+                {
+                    ws.Options.SetRequestHeader("Authorization", $"Bearer {ValueMust.NotNull(_authOptions.Token)}");
+                    break;
+                }
+                case AuthorizationMethods.Cookie:
+                {
+                    CookieContainer cookies = ws.Options.Cookies ?? new();
+                    cookies.Add(new Cookie()
+                    {
+                        HttpOnly = true,
+                        Name = "r_session",
+                        Path = "/",
+                        Secure = true,
+                        Value = ValueMust.NotNull(_authOptions.Token),
+                    });
+                    ws.Options.Cookies = cookies;
+                    break;
+                }
+            }
+
+            return ws;
+        }
 
         public void Dispose()
         {
+            _logger?.LogInformation("Disposing the traQ client.");
             _client.Dispose();
             GC.SuppressFinalize(this);
-        }
-
-        public Task StartAsync(CancellationToken ct)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken ct)
-        {
-            return Task.CompletedTask;
         }
     }
 }
--- src/BotTidus.TraqClient/IClientTraqService.cs
+++ src/BotTidus.TraqClient/IClientTraqService.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using System.Net.WebSockets;
 
 namespace BotTidus.TraqClient
 {
+    public enum AuthorizationMethods
+    {
+        None, Bearer, Cookie
+    }
+
     public interface IClientTraqService : IDisposable
     {
+        public Uri BaseUri { get; }
+        public Models.Traq Traq { get; }
+        public ClientWebSocket CreateClientWebSocket();
     }
 }
