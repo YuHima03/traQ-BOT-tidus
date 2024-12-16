62f3ca7be0a454774b0fb9bb44773fde3acaf3f0
Yusuke.T <yuhima03@gmail.com>
2024-12-16 12:47:28 +0900

    ✨ DIを用いてBotを運用するように

--- src/BotTidus.BotClient/Program.cs
+++ src/BotTidus.BotClient/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace BotTidus.BotClient
+﻿using BotTidus.TraqBot;
+using BotTidus.TraqClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BotTidus.BotClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var accessToken = Environment.GetEnvironmentVariable("BOT_ACCESS_TOKEN");
+            if (accessToken is null)
+            {
+                throw new Exception("Environment variable `BOT_ACCESS_TOKEN` does not exist.");
+            }
+
+            var botAppBuilder = Host.CreateApplicationBuilder();
+
+            botAppBuilder.Services
+                .AddHostedService<TraqBotService>()
+                .AddHttpClient()
+                .AddLogging(b => b.AddConsole())
+                .AddSingleton<IClientTraqService, ClientTraqService>();
+
+            var botApp = botAppBuilder.Build();
+            await botApp.RunAsync();
         }
     }
 }
