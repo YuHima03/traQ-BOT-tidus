042e7218cfab0f142fd6b1967d32a33f9977ab03
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:44:54 +0900

    ✨ traQ内のオブジェクトのモデル

---
+++ src/BotTidus.TraqClient/Models/Traq.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTidus.TraqClient.Models
+{
+    public sealed class Traq(HttpClient client)
+    {
+        readonly HttpClient _client = client;
+
+        public required Uri BaseUri { get; init; }
+
+        public ChannelList PublicChannels { get; } = new ChannelList(client);
+    }
+}
