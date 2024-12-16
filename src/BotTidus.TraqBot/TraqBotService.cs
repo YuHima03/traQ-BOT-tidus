2773e047a18b29fd637e35ba880ae14b14644025
YuHima03 <yuhima03@gmail.com>
2024-12-17 00:48:08 +0900

    ✨ TraqBotService クラスで WebSocket 通信をする

--- src/BotTidus.TraqBot/TraqBotService.cs
+++ src/BotTidus.TraqBot/TraqBotService.cs
@@ -2,30 +2,73 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Net.WebSockets;
 
 namespace BotTidus.TraqBot
 {
-    public class TraqBotService(IServiceProvider services) : BackgroundService
+    public class TraqBotService : BackgroundService
     {
-        readonly ILogger<TraqBotService> _logger = services.GetRequiredService<ILogger<TraqBotService>>();
+        readonly ILogger<TraqBotService>? _logger;
+        readonly IClientTraqService _traq;
+        readonly ClientWebSocket _ws;
 
-        readonly IClientTraqService _traq = services.GetRequiredService<IClientTraqService>();
+        public TraqBotService(IServiceProvider provider)
+        {
+            _logger = provider.GetService<ILogger<TraqBotService>>();
+            _traq = provider.GetRequiredService<IClientTraqService>();
+            _ws = _traq.CreateClientWebSocket();
+        }
 
-        protected override Task ExecuteAsync(CancellationToken ct)
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             return Task.CompletedTask;
         }
 
-        public override Task StartAsync(CancellationToken ct)
+        protected virtual ValueTask OnMessageCreated(CancellationToken ct)
         {
-            _logger.LogInformation("Starting a traQ bot.");
-            return base.StartAsync(ct);
+            return ValueTask.CompletedTask;
         }
 
-        public override Task StopAsync(CancellationToken ct)
+        async ValueTask StartWebSocketAsync(CancellationToken ct)
         {
-            _logger.LogInformation("Stopping the traQ bot.");
-            return base.StopAsync(ct);
+            var ws = _ws;
+            var baseUri = _traq.BaseUri;
+            var logger = _logger;
+
+            try
+            {
+                Uri wsUri = new(baseUri, "api/v3/bots/ws");
+                Uri uri = baseUri.Scheme switch
+                {
+                    "http" => new($"ws://{wsUri.Host}{wsUri.AbsolutePath}"),
+                    "https" => new($"wss://{wsUri.Host}{wsUri.AbsolutePath}"),
+                    _ => throw new Exception("Unknown scheme of the baseUri.")
+                };
+                await ws.ConnectAsync(uri, ct);
+            }
+            catch(Exception e)
+            {
+                logger?.LogError(e, "Failed to connect to a WebSocket server.");
+                throw;
+            }
+            logger?.LogInformation("Successfully connected to a WebSocket server.");
+        }
+
+        public override async Task StartAsync(CancellationToken ct)
+        {
+            await base.StartAsync(ct);
+            await StartWebSocketAsync(ct);
+            return;
+        }
+
+        public override async Task StopAsync(CancellationToken ct)
+        {
+            await base.StopAsync(ct);
+
+            if (_ws.State == WebSocketState.Open)
+            {
+                await _ws.CloseAsync(WebSocketCloseStatus.NormalClosure, null, ct);
+            }
         }
     }
 }
