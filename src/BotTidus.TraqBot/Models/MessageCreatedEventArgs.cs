576d6f2f5298066d3deab41255a7f2804aaa6481
YuHima03 <yuhima03@gmail.com>
2024-12-17 21:52:31 +0900

    ✨ MESSAGE_CREATED イベントで使うクラス群

---
+++ src/BotTidus.TraqBot/Models/MessageCreatedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+
+namespace BotTidus.TraqBot.Models
+{
+    public sealed class MessageCreatedEventArgs
+    {
+        [JsonPropertyName("eventTime")]
+        public DateTime DispatchedAt { get; set; }
+
+        [JsonPropertyName("message")]
+        [NotNull]
+        public MessageEventMessage? Message { get; set; }
+    }
+}
