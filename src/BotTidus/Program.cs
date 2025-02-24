e8c69dde532dcd506af457f69ea03b27b3c5370f
YuHima03 <yuhima03@gmail.com>
2025-02-24 23:16:05 +0900

    ✨ 環境変数の読み込み

--- src/BotTidus/Program.cs
+++ src/BotTidus/Program.cs
@@ -1,40 +1,77 @@
-﻿using BotTidus.BotClient.BotServices;
-using BotTidus.TraqClient;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Traq;
 
-namespace BotTidus.BotClient
+namespace BotTidus
 {
     internal class Program
     {
-        static readonly Uri BaseUri = new("https://q.trap.jp/");
-
         static async Task Main(string[] args)
         {
-            var accessToken = Environment.GetEnvironmentVariable("BOT_ACCESS_TOKEN");
-            if (accessToken is null)
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, conf) =>
+                {
+
+                    var envFiles = ctx.Configuration["env-files"]?.Split(';') ?? [];
+                    foreach (var envFile in envFiles)
+                    {
+                        var fs = File.OpenRead(envFile);
+                        conf.AddIniStream(fs);
+                    }
+                })
+                .ConfigureServices((ctx, services) =>
+                {
+                    services.AddDbContextFactory<Repository.RepositoryContext>(ob =>
+                    {
+                        ob.UseMySQL(GetConnectionString(ctx));
+                    });
+
+                    services.AddTraqApiClient(o =>
+                    {
+                        o.BaseAddress = ctx.Configuration["TRAQ_API_BASE_ADDRESS"] ?? "https://q.trap.jp/api/v3/";
+                        o.BearerAuthToken = ctx.Configuration["BOT_ACCESS_TOKEN"];
+                    });
+
+                    services.AddSingleton(TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time"));
+                })
+                .Build();
+
+            using CancellationTokenSource cts = new();
+            await host.RunAsync(cts.Token);
+        }
+
+        private static string GetConnectionString(HostBuilderContext ctx)
+        {
+            var onDocker = ctx.Configuration["ON_DOCKER"] == "true";
+
+            MySqlConnectionStringBuilder csb = new()
             {
-                throw new Exception("Environment variable `BOT_ACCESS_TOKEN` does not exist.");
+                UserID = ctx.Configuration["NS_MARIADB_USER"],
+                Password = ctx.Configuration["NS_MARIADB_PASSWORD"],
+                Database = ctx.Configuration["NS_MARIADB_DATABASE"],
+            };
+
+            if (onDocker)
+            {
+                csb.Server = ctx.Configuration["MARIADB_EXPOSE_HOSTNAME"] ?? ctx.Configuration["NS_MARIADB_HOSTNAME"];
+                if (uint.TryParse(ctx.Configuration["MARIADB_EXPOSE_PORT"], out var _port) || uint.TryParse(ctx.Configuration["NS_MARIADB_PORT"], out _port))
+                {
+                    csb.Port = _port;
+                }
+            }
+            else
+            {
+                csb.Server = ctx.Configuration["NS_MARIADB_HOSTNAME"];
+                if (uint.TryParse(ctx.Configuration["NS_MARIADB_PORT"], out var _port))
+                {
+                    csb.Port = _port;
+                }
             }
 
-            var botAppBuilder = Host.CreateApplicationBuilder();
-
-            botAppBuilder.Services
-                .AddLogging(conf => conf
-                    .AddSimpleConsole(options => {
-                        options.ColorBehavior = Microsoft.Extensions.Logging.Console.LoggerColorBehavior.Enabled;
-                        options.TimestampFormat = "O";
-                    })
-                )
-                .AddTraqClient<ClientTraqService>(conf => conf
-                    .SetBaseUri(BaseUri)
-                    .UseBearerAuthorization(accessToken)
-                )
-                .AddHostedService<InteractiveBotService>();
-
-            var botApp = botAppBuilder.Build();
-            await botApp.RunAsync();
+            return csb.ConnectionString;
         }
     }
 }
