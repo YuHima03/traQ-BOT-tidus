8a66b9c8ffdd738528b6d66f158804c2c506347d
YuHima03 <yuhima03@gmail.com>
2025-02-27 02:13:17 +0900

    ✨ RecentMessageCollectingService サービスクラス

---
+++ src/BotTidus/Services/RecentMessageCollectingService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BotTidus.Services
+{
+    internal abstract class RecentMessageCollectingService(IServiceProvider services, TimeSpan interval) : BackgroundService
+    {
+        readonly ILogger<RecentMessageCollectingService> _logger = services.GetRequiredService<ILogger<RecentMessageCollectingService>>();
+
+        protected Traq.ITraqApiClient Client { get; } = services.GetRequiredService<Traq.ITraqApiClient>();
+
+        public TimeSpan Interval { get; } = interval.Duration();
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using PeriodicTimer timer = new(Interval);
+            DateTimeOffset lastCollectedAt = DateTimeOffset.UtcNow;
+
+            _logger.LogDebug("Started collecting messages.");
+
+            while (await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                var timeline = await Client.ActivityApi.GetActivityTimelineAsync(limit: 50, all: true, cancellationToken: stoppingToken);
+                var messages = timeline.TakeWhile(m => m.CreatedAt > lastCollectedAt).ToArray();
+
+                _logger.LogDebug("Collected {Count} messages.", messages.Length);
+
+                if (messages.Length != 0)
+                {
+                    lastCollectedAt = messages[0].CreatedAt;
+                    await OnCollectAsync(messages, stoppingToken);
+                }
+
+                timeline.Clear();
+            }
+        }
+
+        protected abstract ValueTask OnCollectAsync(Traq.Model.ActivityTimelineMessage[] messages, CancellationToken ct);
+    }
+}
