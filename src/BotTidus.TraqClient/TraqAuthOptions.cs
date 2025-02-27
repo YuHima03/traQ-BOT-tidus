7e25f5d9a2baaf6e894df446f5b6be4edcbd67f0
YuHima03 <yuhima03@gmail.com>
2025-02-28 01:23:14 +0900

    🔥 不要になったプロジェクトを削除

--- src/BotTidus.TraqClient/TraqAuthOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BotTidus.TraqClient
-{
-    internal sealed class TraqAuthOptions
-    {
-        public required AuthorizationMethods AuthorizationMethod { get; init; }
-        public required string? Token { get; init; }
-    }
-}
