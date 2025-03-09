7cb19af53f10d936646a082c3a7e0158877a9938
YuHima03 <yuhima03@gmail.com>
2025-03-09 23:40:13 +0900

    ✨ IMessageFaceScoresRepository インターフェースの実装

---
+++ BotTidus.RepositoryImpl/MessageFaceScoresRepository.cs
@@ -0,0 +1,42 @@
+﻿using BotTidus.Domain.MessageFaceScores;
+using Microsoft.EntityFrameworkCore;
+
+namespace BotTidus.RepositoryImpl
+{
+    public sealed partial class Repository
+    {
+        ValueTask IMessageFaceScoresRepository.AddMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        ValueTask<MessageFaceScore> IMessageFaceScoresRepository.GetMessageFaceScoreAsync(Guid id, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        async ValueTask<MessageFaceScore[]> IMessageFaceScoresRepository.GetMessageFaceScoresByUserIdAsync(Guid userId, CancellationToken ct)
+        {
+            return await MessageFaceScores
+                .Where(r => r.UserId == userId)
+                .Select(r => new MessageFaceScore(r.MessageId, r.UserId, r.NegativePhraseCount, r.NegativeReactionCount, r.PositivePhraseCount, r.PositiveReactionCount))
+                .ToArrayAsync(ct);
+        }
+
+        async ValueTask<KeyValuePair<Guid, int>[]> IMessageFaceScoresRepository.GetUserFaceCountsAsync(CancellationToken ct)
+        {
+            return await MessageFaceScores
+                .GroupBy(s => s.UserId)
+                .Select(g => KeyValuePair.Create(
+                    g.Key,
+                    g.Select(s => (int)(s.PositivePhraseCount + s.PositiveReactionCount) - (int)(s.NegativePhraseCount + s.NegativeReactionCount)).Sum()
+                    ))
+                .ToArrayAsync(ct);
+        }
+
+        ValueTask IMessageFaceScoresRepository.UpdateMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
