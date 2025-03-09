cbaa0ec2c472efd8f906b372a40e6f1d83598806
YuHima03 <yuhima03@gmail.com>
2025-03-10 01:47:46 +0900

    🏷️ UserFaceCount クラスを戻り値にする

--- BotTidus.Domain/MessageFaceScores/IMessageFaceScoresRepository.cs
+++ BotTidus.Domain/MessageFaceScores/IMessageFaceScoresRepository.cs
@@ -5,7 +5,7 @@
         public ValueTask AddMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct);
         public ValueTask<MessageFaceScore> GetMessageFaceScoreAsync(Guid id, CancellationToken ct);
         public ValueTask<MessageFaceScore[]> GetMessageFaceScoresByUserIdAsync(Guid userId, CancellationToken ct);
-        public ValueTask<KeyValuePair<Guid, int>[]> GetUserFaceCountsAsync(CancellationToken ct);
+        public ValueTask<UserFaceCount[]> GetUserFaceCountsAsync(CancellationToken ct);
         public ValueTask UpdateMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct);
     }
 }
---
+++ BotTidus.Domain/MessageFaceScores/UserFaceCount.cs
@@ -0,0 +1,13 @@
+﻿namespace BotTidus.Domain.MessageFaceScores
+{
+    public sealed record class UserFaceCount(
+        Guid UserId,
+        uint NegativePhraseCount,
+        uint NegativeReactionCount,
+        uint PositivePhraseCount,
+        uint PositiveReactionCount
+        )
+    {
+        public int TotalScore => 1 + (int)(PositivePhraseCount + PositiveReactionCount) - (int)(NegativePhraseCount + NegativeReactionCount);
+    }
+}
