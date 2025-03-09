41de328f38f55b01e945b55488e9aad11827f8a7
YuHima03 <yuhima03@gmail.com>
2025-03-09 23:39:45 +0900

    ✨ message_face_scores テーブルのORM

---
+++ BotTidus.RepositoryImpl/Repository.cs
@@ -0,0 +1,12 @@
+﻿using BotTidus.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BotTidus.RepositoryImpl
+{
+    public sealed partial class Repository(DbContextOptions options) : DbContext(options), IRepository
+    {
+        [NotNull]
+        DbSet<Models.MessageFaceScore>? MessageFaceScores { get; set; }
+    }
+}
