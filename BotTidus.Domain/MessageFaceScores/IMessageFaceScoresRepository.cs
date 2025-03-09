0c19b57369677a3c0d311ba64d86fc7ad5ed3b76
YuHima03 <yuhima03@gmail.com>
2025-03-09 22:53:39 +0900

    🚚 メソッド名変更 (末尾にAsyncをつけた)

--- BotTidus.Domain/MessageFaceScores/IMessageFaceScoresRepository.cs
+++ BotTidus.Domain/MessageFaceScores/IMessageFaceScoresRepository.cs
@@ -2,10 +2,10 @@
 {
     public interface IMessageFaceScoresRepository
     {
-        public ValueTask AddMessageFaceScore(MessageFaceScore score, CancellationToken ct);
+        public ValueTask AddMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct);
         public ValueTask<MessageFaceScore> GetMessageFaceScoreAsync(Guid id, CancellationToken ct);
         public ValueTask<MessageFaceScore[]> GetMessageFaceScoresByUserIdAsync(Guid userId, CancellationToken ct);
-        public ValueTask<KeyValuePair<Guid, int>[]> GetUserFaceCounts(CancellationToken ct);
-        public ValueTask UpdateMessageFaceScore(MessageFaceScore score, CancellationToken ct);
+        public ValueTask<KeyValuePair<Guid, int>[]> GetUserFaceCountsAsync(CancellationToken ct);
+        public ValueTask UpdateMessageFaceScoreAsync(MessageFaceScore score, CancellationToken ct);
     }
 }
