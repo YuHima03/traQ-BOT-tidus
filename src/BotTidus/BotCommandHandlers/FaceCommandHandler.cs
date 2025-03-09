52b3596214d4353271c3bb2ac21be68ce5507320
YuHima03 <yuhima03@gmail.com>
2025-03-10 00:58:59 +0900

    ✨ face コマンドのハンドラ (一部) 実装

---
+++ src/BotTidus/BotCommandHandlers/FaceCommandHandler.cs
@@ -0,0 +1,113 @@
+﻿using BotTidus.ConsoleCommand;
+using BotTidus.Domain.MessageFaceScores;
+using System.Text;
+using Traq;
+
+namespace BotTidus.BotCommandHandlers
+{
+    struct FaceCommandHandler(IMessageFaceScoresRepository repo, ITraqApiClient traq) : IAsyncConsoleCommandHandler<FaceCommandResult>
+    {
+        IMessageFaceScoresRepository _repo = repo;
+        ITraqApiClient _traq = traq;
+
+        SubCommands? _subCommand;
+        string? _username;
+
+        public bool RequiredArgumentsAreFilled => _subCommand is not null;
+
+        public async ValueTask<FaceCommandResult> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            if (_subCommand is null)
+            {
+                return new() { IsSuccessful = false, ErrorType = CommandErrorType.InvalidArguments };
+            }
+            switch (_subCommand)
+            {
+                case SubCommands.DisplayRanking:
+                {
+                    if (_username is not null)
+                    {
+                        return new() { IsSuccessful = false, ErrorType = CommandErrorType.InvalidArguments };
+                    }
+
+                    var faceCounts = await _repo.GetUserFaceCountsAsync(cancellationToken);
+                    if (faceCounts.Length == 0)
+                    {
+                        return new() { IsSuccessful = true, Message = "まだ誰も顔の増減が無いようです." };
+                    }
+
+                    Array.Sort(faceCounts, (a, b) => a.Value - b.Value);
+                    StringBuilder sb = new("""
+                        顔ランキング
+                        | 順位 | ユーザー | 現在の数 |
+                        | ---: | :------ | -------: |
+                        """);
+
+                    int rank = 1;
+                    int prevCount = 0;
+                    for (int i = 0; i < faceCounts.Length; i++)
+                    {
+                        var current = faceCounts[i];
+                        var user = await _traq.UserApi.GetUserAsync(current.Key, cancellationToken);
+                        sb.AppendLine($"| {(current.Value == prevCount ? null : rank)} | :@{user.Name}: {user.Name} | {current.Value} |");
+                        prevCount = current.Value;
+                        rank++;
+                    }
+
+                    return new() { IsSuccessful = true, Message = sb.ToString() };
+                }
+            }
+            return new() { IsSuccessful = false, ErrorType = CommandErrorType.InvalidArguments };
+        }
+
+        public bool TryReadArguments(ConsoleCommandReader reader)
+        {
+            if (!reader.NextValueOnly(out var subcommand))
+            {
+                return false;
+            }
+            _subCommand = subcommand switch
+            {
+                "count" => SubCommands.DisplayCount,
+                "rank" => SubCommands.DisplayRanking,
+                _ => SubCommands.Unknown
+            };
+
+            while (reader.NextArgument(out var arg))
+            {
+                if (arg.HasName && arg.HasValue)
+                {
+                    switch (arg.Name)
+                    {
+                        case "--user":
+                        case "-u":
+                            if (_username is not null)
+                            {
+                                return false;
+                            }
+                            _username = arg.Value.ToString();
+                            break;
+                    }
+                }
+                return false;
+            }
+            return true;
+        }
+
+        enum SubCommands : byte
+        {
+            Unknown = 0,
+            DisplayCount,
+            DisplayRanking
+        }
+    }
+
+    readonly struct FaceCommandResult : ICommandResult
+    {
+        public CommandErrorType ErrorType { get; init; }
+        public bool IsSuccessful { get; init; }
+        public string? Message { get; init; }
+
+        public override string? ToString() => Message;
+    }
+}
