14ab1f8084017d264a7c354c06dd0c26a7bea0d2
YuHima03 <yuhima03@gmail.com>
2025-03-13 22:12:47 +0900

    ✨ TraqCacheHelper クラス

---
+++ src/BotTidus/Helpers/TraqCacheHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace BotTidus.Helpers
+{
+    static class TraqCacheHelper
+    {
+        static readonly TimeSpan Expiration = TimeSpan.FromMinutes(3);
+
+        public static async ValueTask<Traq.Model.UserDetail> GetCachedUserAsync(this Traq.Api.IUserApi api, Guid id, IMemoryCache cache, CancellationToken ct)
+        {
+            if (cache.TryGetValue<Traq.Model.UserDetail>($"traq.user[{id}]", out var user))
+            {
+                return user!;
+            }
+            user = await api.GetUserAsync(id, ct);
+            cache.CreateEntry($"traq.user[{id}]").SetValue(user).SetAbsoluteExpiration(Expiration);
+            return user;
+        }
+    }
+}
