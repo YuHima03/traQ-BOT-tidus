e7aa09ce02ed10d60242827927a5299ac1ba6ff2
YuHima03 <yuhima03@gmail.com>
2025-04-07 00:49:12 +0900

    ✨ WakaruMessageRankingService クラス

---
+++ src/BotTidus/Services/WakaruMessageRanking/WakaruMessageRankingService.cs
@@ -0,0 +1,14 @@
+﻿using BotTidus.Helpers;
+using Traq.Model;
+
+namespace BotTidus.Services.WakaruMessageRanking
+{
+    internal class WakaruMessageRankingService(IServiceProvider services)
+        : DailyMessageCollectingService(services, TimeHelper.GetTimeSpanUntilNextTime(TimeOnly.MinValue)) // Wait until next 09:00:00(JST)
+    {
+        protected override ValueTask OnCollectAsync(Message[] messages, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
