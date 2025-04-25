9036ef763ce5c0bc805ec4fd8712b52ac7bf0dbf
YuHima03 <yuhima03@gmail.com>
2025-04-25 10:26:58 +0900

    ✨ ConsoleCommandParseResult 型

---
+++ src/BotTidus/ConsoleCommand/ConsoleCommandParseResult.cs
@@ -0,0 +1,32 @@
+﻿namespace BotTidus.ConsoleCommand
+{
+    readonly struct ConsoleCommandParseResult<TResult>
+    {
+        public string? Error { get; init; }
+
+        public bool IsSuccessful { get; init; }
+
+        public string? Message { get; init; }
+
+        public TResult Result { get; init; }
+
+        public static ConsoleCommandParseResult<TResult> CreateSuccessful(TResult result)
+        {
+            return new ConsoleCommandParseResult<TResult>
+            {
+                IsSuccessful = true,
+                Result = result
+            };
+        }
+
+        public static ConsoleCommandParseResult<TResult> CreateFailed(string error, string? message)
+        {
+            return new ConsoleCommandParseResult<TResult>
+            {
+                IsSuccessful = false,
+                Error = error,
+                Message = message
+            };
+        }
+    }
+}
