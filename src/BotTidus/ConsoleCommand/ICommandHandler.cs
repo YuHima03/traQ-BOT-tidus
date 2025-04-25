6a777598c1eefdc12d95d012865456cf5db6da92
YuHima03 <yuhima03@gmail.com>
2025-04-25 10:27:20 +0900

    ✨ ICommandHandler インターフェース

---
+++ src/BotTidus/ConsoleCommand/ICommandHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace BotTidus.ConsoleCommand
+{
+    internal interface ICommandHandler<TArgs, out TResult>
+        where TResult : ICommandResult
+    {
+        public abstract static bool TryReadArguments(ConsoleCommandReader reader, out ConsoleCommandParseResult<TArgs> result);
+
+        public abstract static TResult ExecuteAsync(TArgs args, CancellationToken cancellationToken);
+    }
+}
