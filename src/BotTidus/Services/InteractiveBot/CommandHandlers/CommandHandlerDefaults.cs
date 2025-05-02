50f49f29db44893352b3365b21be311268e7ae82
YuHima03 <yuhima03@gmail.com>
2025-05-02 15:52:30 +0900

    ✨ CommandHandler 関連のデフォルト型

---
+++ src/BotTidus/Services/InteractiveBot/CommandHandlers/CommandHandlerDefaults.cs
@@ -0,0 +1,46 @@
+﻿using BotTidus.ConsoleCommand;
+
+namespace BotTidus.Services.InteractiveBot.CommandHandlers
+{
+    readonly struct DefaultCommandArguments { }
+
+    readonly struct DefaultCommandResult : ICommandResult
+    {
+        public string? Error { get; init; }
+
+        public bool IsSuccessful { get; init; }
+
+        public string? Message { get; init; }
+
+        public override string ToString()
+        {
+            if (IsSuccessful)
+            {
+                return Message ?? string.Empty;
+            }
+            else
+            {
+                return $"Error({Error}): {Message}";
+            }
+        }
+
+        public static DefaultCommandResult CreateSuccessful(string? message)
+        {
+            return new DefaultCommandResult
+            {
+                IsSuccessful = true,
+                Message = message
+            };
+        }
+
+        public static DefaultCommandResult CreateFailed(string error, string? message)
+        {
+            return new DefaultCommandResult
+            {
+                IsSuccessful = false,
+                Error = error,
+                Message = message
+            };
+        }
+    }
+}
