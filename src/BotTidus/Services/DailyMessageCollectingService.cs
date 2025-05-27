580c8ce1dd0f48607d1a08eb9eaffebf5668a34b
YuHima03 <yuhima03@gmail.com>
2025-05-28 03:10:02 +0900

    ♻️ DailyMessageCollectingService の基底クラスを PeriodicBackgroundService へ

--- src/BotTidus/Services/DailyMessageCollectingService.cs
+++ src/BotTidus/Services/DailyMessageCollectingService.cs
@@ -2,23 +2,22 @@
 using BotTidus.Services.ExternalServiceHealthCheck;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Traq;
 
 namespace BotTidus.Services
 {
-    internal abstract class DailyMessageCollectingService : BackgroundService
+    internal abstract class DailyMessageCollectingService : PeriodicBackgroundService
     {
         readonly IMemoryCache _cache;
         readonly ILogger<DailyMessageCollectingService> _logger;
-        readonly TimeSpan _startDelay;
         readonly ITraqApiClient _traq;
         readonly TraqHealthCheckPublisher _traqHealthCheck;
 
         static readonly TimeZoneInfo TimeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
 
-        public DailyMessageCollectingService(IServiceProvider services, TimeSpan startDelay)
+        public DailyMessageCollectingService(IServiceProvider services, TimeSpan startDelay) : base(Options.Create(new PeriodicBackgroundServiceOptions { Delay = startDelay, Period = TimeSpan.FromDays(1) }))
         {
             if (startDelay < TimeSpan.Zero)
             {
@@ -26,43 +25,35 @@ namespace BotTidus.Services
             }
             _cache = services.GetRequiredService<IMemoryCache>();
             _logger = services.GetRequiredService<ILogger<DailyMessageCollectingService>>();
-            _startDelay = startDelay;
             _traq = services.GetRequiredService<ITraqApiClient>();
             _traqHealthCheck = services.GetRequiredService<TraqHealthCheckPublisher>();
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async ValueTask ExecuteCoreAsync(CancellationToken ct)
         {
-            await Task.Delay(_startDelay, stoppingToken);
+            var yesterdayStart = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo).Date.AddDays(-1);
+            var yesterdayEnd = yesterdayStart.AddDays(1).AddTicks(-1);
 
-            PeriodicTimer timer = new(TimeSpan.FromDays(1));
-            do
+            if (_traqHealthCheck.CurrentStatus != TraqStatus.Available)
             {
-                var yesterdayStart = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo).Date.AddDays(-1);
-                var yesterdayEnd = yesterdayStart.AddDays(1).AddTicks(-1);
-
-                if (_traqHealthCheck.CurrentStatus != TraqStatus.Available)
-                {
-                    _logger.LogWarning("The task (stamp ranking {Date:M/d}) is skipped because the traQ service is not available.", yesterdayStart);
-                    continue;
-                }
-
-                try
-                {
-                    var cacheKey = $"services.dailyMessageCollector:{yesterdayStart:yyyyMMdd}";
-                    if (!_cache.TryGetValue(cacheKey, out Traq.Model.Message[]? messages) || messages is null)
-                    {
-                        messages = [.. await _traq.MessageApi.SearchManyMessagesAsync(new() { After = TimeZoneInfo.ConvertTimeToUtc(yesterdayStart, TimeZoneInfo), Before = TimeZoneInfo.ConvertTimeToUtc(yesterdayEnd, TimeZoneInfo) }, stoppingToken)];
-                        _cache.Set(cacheKey, messages, TimeSpan.FromMinutes(5));
-                    }
-                    await OnCollectAsync(messages, stoppingToken);
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e, "Failed to collect messages.");
-                }
+                _logger.LogWarning("The task (stamp ranking {Date:M/d}) is skipped because the traQ service is not available.", yesterdayStart);
+                return;
             }
-            while (await timer.WaitForNextTickAsync(stoppingToken));
+
+            try
+            {
+                var cacheKey = $"services.dailyMessageCollector:{yesterdayStart:yyyyMMdd}";
+                if (!_cache.TryGetValue(cacheKey, out Traq.Model.Message[]? messages) || messages is null)
+                {
+                    messages = [.. await _traq.MessageApi.SearchManyMessagesAsync(new() { After = TimeZoneInfo.ConvertTimeToUtc(yesterdayStart, TimeZoneInfo), Before = TimeZoneInfo.ConvertTimeToUtc(yesterdayEnd, TimeZoneInfo) }, ct)];
+                    _cache.Set(cacheKey, messages, TimeSpan.FromMinutes(5));
+                }
+                await OnCollectAsync(messages, ct);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to collect messages.");
+            }
         }
 
         protected abstract ValueTask OnCollectAsync(Traq.Model.Message[] messages, CancellationToken ct);
