5a0e713a78f44e5957e663b482b287c9c863fc40
YuHima03 <yuhima03@gmail.com>
2025-05-28 03:05:09 +0900

    ✨ PeriodicBackgroundService

---
+++ src/BotTidus/Services/PeriodicBackgroundService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace BotTidus.Services
+{
+    abstract class PeriodicBackgroundService(
+        IOptions<PeriodicBackgroundServiceOptions> options
+        ) : BackgroundService
+    {
+        readonly PeriodicBackgroundServiceOptions _options = options.Value;
+
+        protected sealed override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                await Task.Delay(_options.Delay, stoppingToken);
+                using PeriodicTimer timer = new(_options.Period);
+                do
+                {
+                    await ExecuteCoreAsync(stoppingToken);
+                }
+                while (await timer.WaitForNextTickAsync(stoppingToken));
+            }
+            catch (OperationCanceledException)
+            {
+                return;
+            }
+        }
+
+        protected abstract ValueTask ExecuteCoreAsync(CancellationToken ct);
+    }
+
+    class PeriodicBackgroundServiceOptions
+    {
+        public TimeSpan Delay { get; set; }
+
+        public TimeSpan Period { get; set; }
+    }
+}
