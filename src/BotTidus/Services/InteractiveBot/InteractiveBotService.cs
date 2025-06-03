8d2439108cc9f35551023ee0f95502ba067767fe
YuHima03 <yuhima03@gmail.com>
2025-06-04 07:04:49 +0900

    🔥 不要な定数フィールドを削除

--- src/BotTidus/Services/InteractiveBot/InteractiveBotService.cs
+++ src/BotTidus/Services/InteractiveBot/InteractiveBotService.cs
@@ -35,11 +35,6 @@ namespace BotTidus.Services.InteractiveBot
         readonly TraqBotOptions _botOptions = botOptions.Value;
         readonly ILogger<InteractiveBotService> _logger = loggers.CreateLogger<InteractiveBotService>();
 
-        public static readonly Guid StampId_Explosion = new("27475336-812d-4040-9c0e-c7367cd1c966");        // explosion
-        public static readonly Guid StampId_Question = new("408b504e-89c1-474b-abfb-16779a3ee595");         // question
-        public static readonly Guid StampId_Success = new("93d376c3-80c9-4bb2-909b-2bbe2fbf9e93");          // white_check_mark
-        public static readonly Guid StampId_PermissionDenied = new("544c04db-9cc3-4c0e-935d-571d4cf103a2"); // no_entry_sign
-
         protected override ValueTask InitializeAsync(CancellationToken ct)
         {
             _logger.LogInformation("Command prefix: {Prefix}", _botOptions.CommandPrefix);
