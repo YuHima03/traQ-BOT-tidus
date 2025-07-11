c7db0d08658a787d5dcfc0c788d7dfc46065c157
YuHima03 <yuhima03@gmail.com>
2025-07-12 01:39:40 +0900

    ✨ DiscordWebhooksRepository

---
+++ src/BotTidus.Domain/DiscordWebhook/IDiscordWebhooksRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BotTidus.Domain.DiscordWebhook
+{
+    public interface IDiscordWebhooksRepository
+    {
+        public ValueTask<DiscordWebhook[]> GetDiscordWebhooksAsync(bool includeDisabled, CancellationToken ct);
+    }
+}
---
+++ src/BotTidus.RepositoryImpl/DiscordWebhooksRepository.cs
@@ -0,0 +1,18 @@
+﻿using BotTidus.Domain.DiscordWebhook;
+using Microsoft.EntityFrameworkCore;
+
+namespace BotTidus.RepositoryImpl
+{
+    sealed partial class Repository : IDiscordWebhooksRepository
+    {
+        public async ValueTask<DiscordWebhook[]> GetDiscordWebhooksAsync(bool includeDisabled, CancellationToken ct)
+        {
+            var q = includeDisabled switch
+            {
+                true => DiscordWebhooks.AsQueryable(),
+                false => DiscordWebhooks.Where(x => x.IsEnabled)
+            };
+            return await q.Select(x => x.ToDomainObject()).ToArrayAsync(ct).ConfigureAwait(false);
+        }
+    }
+}
