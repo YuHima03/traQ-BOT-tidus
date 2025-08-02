bd89f6c16a826a771e0a6d5b089e5f61b5154e36
YuHima03 <yuhima03@gmail.com>
2025-08-03 02:53:47 +0900

    🔥 不要な属性を削除

--- src/BotTidus.RepositoryImpl/Repository.cs
+++ src/BotTidus.RepositoryImpl/Repository.cs
@@ -1,15 +1,12 @@
 ﻿using BotTidus.Domain;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics.CodeAnalysis;
 
 namespace BotTidus.RepositoryImpl
 {
     public sealed partial class Repository(DbContextOptions<Repository> options) : DbContext(options), IRepository
     {
-        [NotNull]
-        DbSet<Models.MessageFaceScore>? MessageFaceScores { get; set; }
+        DbSet<Models.MessageFaceScore> MessageFaceScores { get; set; }
 
-        [NotNull]
-        DbSet<Models.DiscordWebhook>? DiscordWebhooks { get; set; }
+        DbSet<Models.DiscordWebhook> DiscordWebhooks { get; set; }
     }
 }
