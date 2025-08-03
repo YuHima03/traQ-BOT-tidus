9f65cc1cb7d6f68caa1b4b810bc8bd4f979c8f1d
YuHima03 <yuhima03@gmail.com>
2025-08-03 20:24:49 +0900

    ✨ BotDbContextDesignTimeFactory

---
+++ src/BotTidus.Infrastructure/Repository/BotDbContextDesignTimeFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BotTidus.Infrastructure.Repository
+{
+    public sealed class BotDbContextDesignTimeFactory : IDesignTimeDbContextFactory<BotDbContext>
+    {
+        public BotDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<BotDbContext> optionsBuilder = new();
+            optionsBuilder.UseMySql(MariaDbServerVersion.LatestSupportedServerVersion);
+            return new(optionsBuilder.Options);
+        }
+    }
+}
