054f9b528bca56ae3774ada099641678520cbfce
YuHima03 <yuhima03@gmail.com>
2025-08-03 20:35:04 +0900

    ✨ Infrastructure.Repository.BotDbContext に移行

--- src/BotTidus/Program.cs
+++ src/BotTidus/Program.cs
@@ -72,7 +72,7 @@ namespace BotTidus
                         .AddObjectPool<Traq.Models.PostMessageStampRequest>();
 
                     services.AddHealthChecks()
-                        .AddMySqlWithDbContext<RepositoryImpl.Repository>()
+                        .AddMySqlWithDbContext<Infrastructure.Repository.BotDbContext>()
                         .AddTypedHostedService<FaceCollectingService>()
                         .AddTypedHostedService<FaceReactionCollectingService>()
                         .AddTypedHostedService<StampRankingService>()
@@ -82,7 +82,7 @@ namespace BotTidus
                     services.AddSingleton<HealthCheckPublisher>().AddSingleton<IHealthCheckPublisher, HealthCheckPublisher>(static sp => sp.GetRequiredService<HealthCheckPublisher>());
                     services.AddSingleton<TraqHealthCheckPublisher>();
 
-                    services.AddDbContextFactory<RepositoryImpl.Repository>((sp, ob) =>
+                    services.AddDbContextFactory<Infrastructure.Repository.BotDbContext>((sp, ob) =>
                     {
                         var connStr = sp.GetRequiredService<IOptions<DbConnectionOptions>>().Value.GetConnectionString();
                         ob.UseMySql(connStr, MySqlServerVersion.LatestSupportedServerVersion);
@@ -92,7 +92,6 @@ namespace BotTidus
                             ob.EnableSensitiveDataLogging();
                         }
                     });
-                    services.AddSingleton<IRepositoryFactory, RepositoryImpl.RepositoryFactory>(sp => new(sp.GetRequiredService<IDbContextFactory<RepositoryImpl.Repository>>()));
 
                     services.AddSingleton(GetDefaultTimeZoneInfo(ctx.Configuration));
 
